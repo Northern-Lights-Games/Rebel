ddeb1d90a86117d4f665842222fe5a45cc2d635e
skyloft7 <skyloftmb@gmail.com>
2023-11-19 10:16:40 +0530

    more colors

--- src/Color.java
+++ src/Color.java
@@ -4,9 +4,10 @@ public class Color {
     public static final Color RED = new Color(1f, 0f, 0f, 1f);
     public static final Color GREEN = new Color(0f, 1f, 0f, 1f);
     public static final Color BLUE = new Color(0f, 0f, 1f, 1f);
-
     public static final Color BLACK = new Color(0f, 0f, 0f, 1f);
     public static final Color WHITE = new Color(1f, 1f, 1f, 1f);
+    public static final Color GRAY = new Color(0.5f, 0.5f, 0.5f, 1f);
+    public static final Color LIGHT_GRAY = new Color(0.8f, 0.8f, 0.8f, 1f);
 
 
 
