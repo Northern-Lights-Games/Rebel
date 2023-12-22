c1ad61aafbd84fdd7b3035e6c7f9b684d207a24f
skyloft7 <skyloftmb@gmail.com>
2023-12-22 09:13:43 +0530

    conversion from RGB to floating point colors

--- src/rebel/graphics/Color.java
+++ src/rebel/graphics/Color.java
@@ -14,12 +14,17 @@ public class Color {
     public static final Color GRAY = new Color(0.5f, 0.5f, 0.5f, 1f);
     public static final Color LIGHT_GRAY = new Color(0.8f, 0.8f, 0.8f, 1f);
 
-
-
     public Color(float r, float g, float b, float a) {
         this.r = r;
         this.g = g;
         this.b = b;
         this.a = a;
     }
+
+    public Color fromRGB(float r, float g, float b, float a){
+        float scale = 1 / 255f;
+        return new Color(scale * r, scale * g, scale * b, scale * a);
+    }
+
+
 }
