36a207f365e5f3bc46d46b28a962f84d0d69fc14
skyloft7 <skyloftmb@gmail.com>
2023-12-23 10:09:45 +0530

    removed call to OpenGL culling, this accidentally made it's way in a previous commit

--- src/rebel/graphics/Renderer2D.java
+++ src/rebel/graphics/Renderer2D.java
@@ -35,7 +35,8 @@ public class Renderer2D {
         this.height = height;
 
         glEnable(GL_TEXTURE_2D);
-        glEnable(GL_CULL_FACE);
+
+
 
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
