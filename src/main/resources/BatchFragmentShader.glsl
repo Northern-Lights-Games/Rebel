ae547514967690ab896da219ee7debe40578110c
skyloft7 <skyloftmb@gmail.com>
2023-12-27 13:38:51 +0530

    Changed shader to avoid using sampler2D indexing. This breaks some drivers.

--- src/main/resources/BatchFragmentShader.glsl
+++ src/main/resources/BatchFragmentShader.glsl
@@ -16,6 +16,7 @@ void main()
     int index = int(f_texindex);
 
 
+
     if(index == -1){
         FragColor = f_color;
     }
@@ -31,7 +32,38 @@ void main()
 
     }
     else {
-        FragColor = texture(u_textures[index], f_uv) * f_color;
+        if(index == 0) FragColor = texture(u_textures[0], f_uv) * f_color;
+        if(index == 1) FragColor = texture(u_textures[1], f_uv) * f_color;
+        if(index == 2) FragColor = texture(u_textures[2], f_uv) * f_color;
+        if(index == 3) FragColor = texture(u_textures[3], f_uv) * f_color;
+        if(index == 4) FragColor = texture(u_textures[4], f_uv) * f_color;
+        if(index == 5) FragColor = texture(u_textures[5], f_uv) * f_color;
+        if(index == 6) FragColor = texture(u_textures[6], f_uv) * f_color;
+        if(index == 7) FragColor = texture(u_textures[7], f_uv) * f_color;
+        if(index == 8) FragColor = texture(u_textures[8], f_uv) * f_color;
+        if(index == 9) FragColor = texture(u_textures[9], f_uv) * f_color;
+        if(index == 10) FragColor = texture(u_textures[10], f_uv) * f_color;
+        if(index == 11) FragColor = texture(u_textures[11], f_uv) * f_color;
+        if(index == 12) FragColor = texture(u_textures[12], f_uv) * f_color;
+        if(index == 13) FragColor = texture(u_textures[13], f_uv) * f_color;
+        if(index == 14) FragColor = texture(u_textures[14], f_uv) * f_color;
+        if(index == 15) FragColor = texture(u_textures[15], f_uv) * f_color;
+        if(index == 16) FragColor = texture(u_textures[16], f_uv) * f_color;
+        if(index == 17) FragColor = texture(u_textures[17], f_uv) * f_color;
+        if(index == 18) FragColor = texture(u_textures[18], f_uv) * f_color;
+        if(index == 19) FragColor = texture(u_textures[19], f_uv) * f_color;
+        if(index == 20) FragColor = texture(u_textures[20], f_uv) * f_color;
+        if(index == 21) FragColor = texture(u_textures[21], f_uv) * f_color;
+        if(index == 22) FragColor = texture(u_textures[22], f_uv) * f_color;
+        if(index == 23) FragColor = texture(u_textures[23], f_uv) * f_color;
+        if(index == 24) FragColor = texture(u_textures[24], f_uv) * f_color;
+        if(index == 25) FragColor = texture(u_textures[25], f_uv) * f_color;
+        if(index == 26) FragColor = texture(u_textures[26], f_uv) * f_color;
+        if(index == 27) FragColor = texture(u_textures[27], f_uv) * f_color;
+        if(index == 28) FragColor = texture(u_textures[28], f_uv) * f_color;
+        if(index == 29) FragColor = texture(u_textures[29], f_uv) * f_color;
+        if(index == 30) FragColor = texture(u_textures[30], f_uv) * f_color;
+        if(index == 31) FragColor = texture(u_textures[31], f_uv) * f_color;
     }
 
 }
